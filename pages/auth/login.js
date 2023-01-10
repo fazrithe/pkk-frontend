3e0bc3de1024e63ae7ea71f37f21b3635460ddce
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-01-10 10:55:43 +0700

    Form login
    Form login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,3 +1,36 @@
+import "bulma/css/bulma.css";
+
 export default function Login(){
-    return "Login";
+    return (
+        <section className="hero has-background-grey-light is fullheight is-fullwidth">
+            <div className="hero-body">
+                <div className="container">
+                    <div className="columns is-centered">
+                        <div className="column is-4-desktop">
+                            <form className="box">
+                                <div className="field mt-5">
+                                    <label className="label">Email or Username</label>
+                                    <div className="controls">
+                                        <input type="text" className="input" placeholder="Username"></input>
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <label className="label">Password</label>
+                                    <div className="controls">
+                                        <input type="password" className="input" placeholder="*****"></input>
+                                    </div>
+                                </div>
+                                <div className="field mt-5">
+                                    <button className="button is-success is-fullwidth">Login</button>
+                                </div>
+                                <div className="field mt-5">
+                                    <a href="/auth/register">Register</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
