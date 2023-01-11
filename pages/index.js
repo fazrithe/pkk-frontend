fd2c26768030241cfa80761b04fd7dbc577c0559
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-01-11 11:53:58 +0700

    add section
    add section

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,33 @@ export default function Home({ allPostsData }) {
         </p>
       </div>
     </section>
+    <section className="container pt-9">
+      <div class="grid justify-items-center pb-9">
+        <div class="box-border md:box-content w-62">
+            <h3 className="title is-4">Kelola Kinerja dan Pengembangan Karir Pegawai Secara Aktual dan Tranparan</h3>
+          </div>
+      </div>
+      <div class="columns is-variable is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd">
+        <div class="column">
+          <div class="content is-small">
+            <h1 className="title">Penyelarasan</h1>
+            <label className="subtitle">Mentor Kinerja Pegawai Agar Sesuai Kinerja.</label>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content is-small">
+            <h1 className="title">Riview Kinerja Objektif</h1>
+            <label className="subtitle">Berbagai metode sistematis untuk evaluasi kinerja yang akurat.</label>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content is-small">
+            <h1 className="title">Pegembangan Karir</h1>
+            <label className="subtitle">Peningkatan jabatan dapat direncanakan secara komprehensif</label>
+          </div>
+        </div>
+      </div>
+    </section>
     </div>
     </>
     )
